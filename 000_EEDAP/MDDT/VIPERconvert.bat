0dd0d86c85cb974572254801c358492edff9fcf7
Brandon Gallas <brandon.gallas@fda.hhs.gov>
2017-01-09 15:15:05 -0500

    Archive the first draft of the MDDT proposal in tex form.

---
+++ 000_EEDAP/MDDT/VIPERconvert.bat
@@ -0,0 +1,3 @@
+pandoc -f docx -t mediawiki -o MDDTproposal-1.wiki MDDTproposal-1.docx
+pandoc -f docx -t latex -o MDDTproposal-1.tex MDDTproposal-1.docx
+pandoc -f docx -t markdown -o MDDTproposal-1.md MDDTproposal-1.docx
