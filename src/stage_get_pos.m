1a560e87f52dfc225845f17093c6937fcfda74ef
kate-elfer <brandon.gallas@fda.hhs.gov>
2024-03-06 13:02:11 -0500

    Fix bugrelatedto adding thorlabs stage functionality

--- src/stage_get_pos.m
+++ src/stage_get_pos.m
@@ -7,26 +7,39 @@ function stage=stage_get_pos(stage,h_stage)
 %
 % objects can be cleared from memory with
 % delete(objects)
+
 try
     stage_label = stage.label;
-    if strcmp(stage_label(end-4:end),'Prior')
+    if strcmp(stage_label,'H101-Prior')
         if exist('h_stage', 'var') == 0
             stage = stage_get_pos_prior(stage);
         else
             stage = stage_get_pos_prior(stage,h_stage);
-        end        
-    elseif
+        end
+        
+    elseif strcmp(stage_label,'SCAN8Praparate_Ludl5000')
         if exist('h_stage', 'var') == 0
             stage = stage_get_pos_Ludl(stage);
         else
             stage = stage_get_pos_Ludl(stage,h_stage);
         end
-    elseif
+        
+    elseif strcmp(stage_label,'BioPrecision2-LE2_Ludl6000')
+        if exist('h_stage', 'var') == 0
+            stage = stage_get_pos_Ludl(stage);
+        else
+            stage = stage_get_pos_Ludl(stage,h_stage);
+        end
+        
+    elseif strcmp(stage_label,'MLS203-ThorLabs')
         if exist('h_stage', 'var') == 0
             stage = stage_get_pos_thorlabs(stage);
         else
             stage = stage_get_pos_thorlabs(stage,h_stage);
         end
+        
+    else
+        error_show(ME)
     end
     
 catch ME
--- src/stage_set_origin.m
+++ src/stage_set_origin.m
@@ -4,19 +4,28 @@
 function stage = stage_set_origin(stage,h_stage)
 try
     stage_label = stage.label;
-    if strcmp(stage_label(end-4:end),'Prior')
+    if strcmp(stage_label,'H101-Prior')
         if exist('h_stage', 'var') == 0
             stage = stage_set_origin_prior(stage);
         else
             stage = stage_set_origin_prior(stage,h_stage);
         end
-    elseif strcmp(stage_label(end-4:end),'Ludl')
+        
+    elseif strcmp(stage_label,'SCAN8Praparate_Ludl5000')
         if exist('h_stage', 'var') == 0
             stage = stage_set_origin_Ludl(stage);
         else
             stage = stage_set_origin_Ludl(stage,h_stage);
         end
-    else
+        
+    elseif strcmp(stage_label,'BioPrecision2-LE2_Ludl6000')
+        if exist('h_stage', 'var') == 0
+            stage = stage_set_origin_Ludl(stage);
+        else
+            stage = stage_set_origin_Ludl(stage,h_stage);
+        end
+        
+    elseif strcmp(stage_label,'MLS203-ThorLabs')
         if exist('h_stage', 'var') == 0
             stage = stage_set_origin_thorlabs(stage);
         else
