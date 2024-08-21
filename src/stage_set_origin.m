86f7c500f89735fc46d69f118702fecbfcc8125e
brandon-gallas <brandon.gallas@fda.hhs.gov>
2024-08-20 23:29:59 -0400

    WIP: rewrite for clarity and robustness

--- src/stage_set_origin.m
+++ src/stage_set_origin.m
@@ -1,37 +1,33 @@
 %  ##########################################################################
 %% ###################### MOVE STAGE SEQUENCE ###############################
 %  ##########################################################################
-function stage = stage_set_origin(stage,h_stage)
+function stage = stage_set_origin(stage)
 try
-    stage_label = stage.label;
-    if strcmp(stage_label,'H101-Prior')
-        if exist('h_stage', 'var') == 0
-            stage = stage_set_origin_prior(stage);
-        else
-            stage = stage_set_origin_prior(stage,h_stage);
+
+            % Set origin
+        switch stage.label
+
+            case 'H101-Prior'
+                stage = stage_set_origin_prior(stage);
+
+            case 'SCAN8Praparate_Ludl5000'
+                stage = stage_set_origin_Ludl(stage);
+
+            case 'SCAN8Praparate_Ludl6000'
+                stage = stage_set_origin_Ludl(stage);
+
+            case 'BioPrecision2-LE2_Ludl5000'
+                stage = stage_set_origin_Ludl(stage);
+
+            case 'BioPrecision2-LE2_Ludl6000'
+                stage = stage_set_origin_Ludl(stage);
+                
+            case 'MLS203-ThorLabs'
+                stage = stage_set_origin_thorlabs(stage);
+
+            otherwise
+                error('The stage label is not recognized')
         end
-        
-    elseif strcmp(stage_label,'SCAN8Praparate_Ludl5000')
-        if exist('h_stage', 'var') == 0
-            stage = stage_set_origin_Ludl(stage);
-        else
-            stage = stage_set_origin_Ludl(stage,h_stage);
-        end
-        
-    elseif strcmp(stage_label,'BioPrecision2-LE2_Ludl6000')
-        if exist('h_stage', 'var') == 0
-            stage = stage_set_origin_Ludl(stage);
-        else
-            stage = stage_set_origin_Ludl(stage,h_stage);
-        end
-        
-    elseif strcmp(stage_label,'MLS203-ThorLabs')
-        if exist('h_stage', 'var') == 0
-            stage = stage_set_origin_thorlabs(stage);
-        else
-            stage = stage_set_origin_thorlabs(stage,h_stage);
-        end    
-    end
 
 catch ME
     error_show(ME);
