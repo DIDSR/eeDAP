7317f4c4d0c1e4b3c03638650e7b9c398bc7d434
brandon-gallas <brandon.gallas@fda.hhs.gov>
2024-08-27 22:00:40 -0400

    Speed up set origin

--- src/stages/Ludl/stage_set_origin_Ludl.m
+++ src/stages/Ludl/stage_set_origin_Ludl.m
@@ -8,20 +8,21 @@
 function stage = stage_set_origin_Ludl(stage)
 try
     
-    % Speed to the top-left corner
-    success = stage_send_com_Ludl (stage.handle, 'SPEED X=200000 Y=200000'); %#ok<*NASGU>
+    % Speed to stage limits
+    success = stage_send_com_Ludl (stage.handle, 'SPEED X=2000000 Y=2000000'); %#ok<*NASGU>
     success = stage_send_com_Ludl (stage.handle, 'ACCEL X=1 Y=1');
     success = stage_send_com_Ludl(stage.handle, 'HOME X Y');
 
     % Wait til the stage gets to the top-left corner
     while stage_check_busy_Ludl(stage.handle)
-        pause(.5)
+        pause(.1)
     end    
 
-    % Set the bottom-right corner to be the origin
+    % Set the the origin
     success = stage_send_com_Ludl(stage.handle, 'HERE X=0 Y=0');
 
-    % For better precision, slow it down, move it away, and go back home
+    % For better precision, 
+    % slow it down, move it away, and go to stage limits
     success = stage_send_com_Ludl(stage.handle, 'SPEED X=100 Y=100');
     success = stage_send_com_Ludl(stage.handle, 'ACCEL X=255 Y=255');
     success = stage_send_com_Ludl(stage.handle, 'MOVE X=100 Y=100');
@@ -30,11 +31,11 @@ try
         pause(.5)
     end
 
-    % Reset the top-left corner to be the origin
+    % Reset the origin
     success = stage_send_com_Ludl (stage.handle, 'HERE X=0 Y=0');
 
     % Back off the origin a little bit
-    stage=stage_move_Ludl(stage,[50000,50000]);
+    stage=stage_move_Ludl(stage, [50000,50000]);
 
 catch ME
     error_show(ME);
